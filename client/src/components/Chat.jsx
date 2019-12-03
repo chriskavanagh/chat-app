19ae849c3f8bbb4e7e2d85a7416a81c5429c9f6c
Chris Kavanagh <ckava3@gmail.com>
2019-12-03 15:04:57 -0500

    Chat.jsx

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,14 +1,48 @@
 import React, { useState, useEffect } from "react";
-//import styled from "styled-components";
+import styled from "styled-components";
 import queryString from "query-string";
 import io from "socket.io-client";
-const ENDPOINT = `localhost:5000`;
+
+const Outerdiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  background-color: #1a1a1d;
+
+  @media (min-width: 320px) and (max-width: 480px) {
+    height: 100%;
+  }
+`;
+
+const Containerdiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  background: #ffffff;
+  border-radius: 8px;
+  height: 60%;
+  width: 35%;
+
+  @media (min-width: 320px) and (max-width: 480px) {
+    width: 100%;
+    height: 100%;
+  }
+
+  @media (min-width: 480px) and (max-width: 1200px) {
+    width: 60%;
+  }
+`;
 
 let socket;
 
 const Chat = ({ location }) => {
   const [name, setName] = useState("");
   const [room, setRoom] = useState("");
+  const [users, setUsers] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+  const ENDPOINT = `localhost:5000`;
 
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
@@ -24,7 +58,35 @@ const Chat = ({ location }) => {
       socket.off();
     };
   }, [ENDPOINT, location.search]);
-  return <h1>Chat Component!</h1>;
+
+  useEffect(() => {
+    socket.on("message", message => {
+      setMessages([...messages, message]);
+    });
+  }, [messages]);
+
+  // send message with Enter Key
+  const sendMessage = e => {
+    e.preventDefault();
+    if (message) {
+      socket.emit("sendMessage", message, () => {
+        setMessage("");
+      });
+    }
+  };
+
+  return (
+    <Outerdiv>
+      <Containerdiv>
+        <h1>Chat</h1>
+        <input
+          value={message}
+          onChange={e => setMessage(e.target.value)}
+          onKeyPress={e => (e.key === "Enter" ? sendMessage(e) : null)}
+        />
+      </Containerdiv>
+    </Outerdiv>
+  );
 };
 
 export default Chat;
